b8c7c73b0895540c9d0ef2959223b55eac23831c
McKinley Forbes <mckinley.forbes95@gmail.com>
2020-10-12 21:26:38 -0400

    file upload and save works now

--- server/routres.js
+++ server/routres.js
@@ -1,31 +1,41 @@
 const express = require("express");
 const Tab = require("./Models/Tab");
 const router = express.Router();
+const multer = require("multer");
+const fs = require('fs');
+const upload = multer();
+
+const path = "./server/tabs/"
 
 router.get("/tabs", async (req, res) => {
     const tabs = await Tab.find();
     res.send(tabs);
 })
 
-router.post("/tabs/new", async (req,res) => {
-    console.log(req.body);
-    /*const tab = new Tab({
+router.post("/tabs/new", upload.array('tab', 1), async (req,res) => {
+    const tabBufffer = req.files[0].buffer;
+    const tab = new Tab({
         title: req.body.title,
         composer: req.body.composer,
-        tab: req.body.tab,
-    });*/
+    });
+    fs.writeFile(`${path}${tab._id}.pdf`, tabBufffer, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+        }
+    })
+    await tab.save();
+    res.send(tab);
 
-    /*await tab.save();
-    res.send(tab);*/
-    res.send({body: "created tab"});
 })
 
-router.get("/post/:id", async (req, res) => {
+router.get("/tabs/:id", async (req, res) => {
     try {
         const tab = await Tab.findOne({
             _id: req.params.id
         })
-        res.send(tab);
+        res.download(`${path}${tab._id}.pdf`, `${tab.title}.pdf`);
     } catch {
         res.status(404);
         res.send({error: "Tab doesn't exist!"})
